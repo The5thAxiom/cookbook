c7c6207e849941eba5cd832a43d600acf817a929
The5thAxiom <samridh.anand.paatni@gmail.com>
2021-11-06 02:35:24 +0530

    proper commenting added

--- Cookbook/__init__.py
+++ Cookbook/__init__.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 # creating the flask app
 app = Flask(__name__)
+
+# creating the db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Cookbook.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # we don't need real time updates as this is a REST based api
-
 db = SQLAlchemy(app)
 
 # this will initialize the tables
