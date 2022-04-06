cebdfce2970371f20a3b073261dfb1ed03eabe15
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-04-06 22:52:51 +0530

    app.run put in main

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import os
 from backend import app
 
-app.run(
-    host='0.0.0.0',
-    debug=False,
-    port=os.environ.get('PORT', 80)
-)
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        debug=False,
+        port=os.environ.get('PORT', 80)
+    )
