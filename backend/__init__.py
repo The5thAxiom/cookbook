bd36ab2b954724bf46cf43298187aa883233726c
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-05-24 12:24:03 +0530

    added flask jwt and authorization endpoints

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,36 @@
+from datetime import timedelta
+
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_jwt_extended import\
+    create_access_token,\
+    get_jwt,\
+    get_jwt_identity,\
+    unset_jwt_cookies,\
+    jwt_required,\
+    JWTManager
 
 # creating the flask app
 app = Flask(__name__, static_folder='../frontend/build', static_url_path='/')
 CORS(app)
+
+app.config["JWT_SECRET_KEY"] = "dummy_key_here"
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=2)
+jwt = JWTManager(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Cookbook.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # we don't need real time updates as this is a REST based api
+
+db = SQLAlchemy(app)
+
+
+# this will initialize the tables
+from backend.models import *
+db.create_all()
+
+from backend.controllers import *
+from backend.endpoints import *
 
 @app.route('/')
 def index():
@@ -13,15 +39,3 @@ def index():
 # @app.errorhandler(404)
 # def not_found(e):
 #     return app.send_static_file('index.html')
-
-# creating the db
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Cookbook.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # we don't need real time updates as this is a REST based api
-db = SQLAlchemy(app)
-
-# this will initialize the tables
-from backend.models import *
-db.create_all()
-
-from backend.controllers import *
-from backend.endpoints import *
--- backend/endpoints.py
+++ backend/endpoints.py
@@ -1,4 +1,13 @@
+from datetime import datetime, timedelta, timezone
+
 from flask import jsonify, request, abort, Response
+from flask_jwt_extended import\
+    create_access_token,\
+    get_jwt,\
+    get_jwt_identity,\
+    unset_jwt_cookies,\
+    jwt_required,\
+    JWTManager
 
 from backend import app
 from backend.models import Recipe
@@ -6,27 +15,53 @@ from backend.controllers import *
 from backend import app
 
 
-@app.route('/api/recipes', methods=['GET', 'POST'])
-def recipes():
-    if request.method == 'POST':
-        newRecipeFull = request.get_json(force=True)
-        if Recipe.query.filter_by(name=newRecipeFull["name"]).first() is not None:
-            return Response(status=202)
-        else:
-            addFullRecipe(newRecipeFull)
-            return Response(status=201)
-    elif request.method == 'GET':
-        return jsonify({"data": "coming soon"})
-
-
 @app.route('/api/users', methods=['GET', 'POST'])
 def users():
     if request.method == 'POST':
-        addNewUser(request.get_json(force=True))
-        return Response(status=201)
+        newUser = request.get_json(force=True)
+        user = User.query.filter(User.username == newUser['username']).first()
+        if user is None:
+            addNewUser(newUser)
+            return Response(status=201)
+        else:
+            return Response(status=201)
+        
     else:
         return jsonify([user.to_dict() for user in User.query.all()])
 
+@app.route('/api/users/login', methods=["POST"])
+def user_login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    user = User.query.filter(User.username == username).first()
+    if password == user.password:
+        access_token = create_access_token(identity=username)
+        return jsonify({"access_token": access_token})
+    else:
+        abort(401)
+
+@app.route('/api/users/logout', methods=["POST"])
+def user_logout():
+    response = Response(status=204)
+    unset_jwt_cookies(response)
+    return response
+
+@app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            data = response.get_json()
+            if type(data) is dict:
+                data["access_token"] = access_token 
+                response.data = json.dumps(data)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
 
 @app.route('/api/users/<username>')
 def users_str(username):
@@ -35,6 +70,17 @@ def users_str(username):
         abort(404)
     else:
         return jsonify(user.to_dict())
+
+
+@app.route('/api/recipes', methods=['POST'])
+@jwt_required()
+def recipes():
+    newRecipeFull = request.get_json(force=True)
+    if Recipe.query.filter_by(name=newRecipeFull["name"]).first() is not None:
+        return Response(status=202)
+    else:
+        addFullRecipe(newRecipeFull)
+        return Response(status=201)
 
 
 @app.route('/api/recipes/<int:num>')
