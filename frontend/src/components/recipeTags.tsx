c4247ada7e6eb29d84851eaee5cd0fcbf57863e1
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-05-26 14:47:32 +0530

    refactored recipe tag links given proper formatting

--- frontend/src/components/recipeTags.tsx
+++ frontend/src/components/recipeTags.tsx
@@ -6,9 +6,14 @@ export default function RecipeTags({ tags }: { tags: string[] }) {
     return (
         <div className='recipe-tags'>
             {tags.map((tag: string, index: number) => (
-                <div key={index} className='recipe-tag'>
-                    <NavLink to={`/recipes?only-tag=${tag}`}>{tag}</NavLink>
-                </div>
+                <NavLink
+                    key={index}
+                    className={'recipe-tag'}
+                    end
+                    to={`/recipes?only-tag=${tag}`}
+                >
+                    {tag}
+                </NavLink>
             ))}
             {/* the 'tag' will be a nav link to a sort-page with the tag filter on (get query probably) */}
         </div>
