126e38bb824538c178e51823a078851602e688e0
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-06-01 11:37:46 +0530

    made recipeCarousel its own component

--- frontend/src/components/recipes/recipeCards.tsx
+++ frontend/src/components/recipes/recipeCards.tsx
@@ -1,57 +1,16 @@
 import React, { useState } from 'react';
 import { recipeMeta } from '../../values/types';
-import BackwardArrowIcon from '../icons/backwardArrowIcon';
-import ForwardArrowIcon from '../icons/forwardArrowIcon';
 import LoadingAnimation from '../loadingAnimation';
 import RecipeCard from './recipeCard';
 import './recipeCards.css';
 
-export default function RecipeCards({
-    recipes,
-    carousel,
-    columns
-}: {
-    recipes: recipeMeta[];
-    carousel?: boolean;
-    columns?: number;
-}) {
+export default function RecipeCards({ recipes }: { recipes: recipeMeta[] }) {
     const [step, setStep] = useState<number>(0);
 
     if (recipes === null) return <LoadingAnimation />;
     else if (recipes.length === 0)
         return <div className='util-centered'>No recipes found :(</div>;
-    else if (carousel && columns) {
-        const carouselNext = () => {
-            setStep(step + columns);
-        };
-        const carouselPrev = () => {
-            setStep(step - columns);
-        };
-        return (
-            <div className='recipe-cards-carousel-container'>
-                {step !== 0 && (
-                    <div className='arrow prev' onClick={carouselPrev}>
-                        <BackwardArrowIcon />
-                    </div>
-                )}
-                <div
-                    className='recipe-cards-carousel'
-                    style={{
-                        gridTemplateColumns: '1fr '.repeat(columns)
-                    }}
-                >
-                    {recipes.slice(step, step + columns).map(r => (
-                        <RecipeCard key={r.id} recipe={r} />
-                    ))}
-                </div>
-                {step + columns <= recipes.length && (
-                    <div className='arrow next' onClick={carouselNext}>
-                        <ForwardArrowIcon />
-                    </div>
-                )}
-            </div>
-        );
-    } else
+    else
         return (
             <div className='recipe-cards'>
                 {recipes.map(r => (
--- frontend/src/pages/users/profile.tsx
+++ frontend/src/pages/users/profile.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import LoadingAnimation from '../../components/loadingAnimation';
-import RecipeCards from '../../components/recipes/recipeCards';
+import RecipeCarousel from '../../components/recipes/recipeCarousel';
 import { recipeMeta, userData } from '../../values/types';
 
 import './profile.css';
@@ -66,7 +66,7 @@ export default function Profile({
                             All recipes
                         </NavLink>
                     </div>
-                    <RecipeCards recipes={recipes} carousel columns={2} />
+                    <RecipeCarousel recipes={recipes} carousel columns={2} />
                 </section>
                 <section id='skills'>
                     {/* <li><NavLink end to='/skills/new'>add skill</NavLink></li>
