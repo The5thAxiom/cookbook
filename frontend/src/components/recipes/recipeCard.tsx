f392b69297f08a26628755ed73fbf0c9b1cc7358
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-06-12 00:33:01 +0530

    recipes can now be added to favourites

--- frontend/src/components/recipes/recipeCard.tsx
+++ frontend/src/components/recipes/recipeCard.tsx
@@ -3,9 +3,11 @@ import React, { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import RecipeTags from './recipeTags';
 import LoadingAnimation from './../loadingAnimation';
+import useCurrentUser from '../../hooks/useCurrentUser';
 
 export default function RecipeCard({ recipe }: { recipe: recipeMeta }) {
     const [tags, setTags] = useState<string[]>(null as any);
+    const [user, fetchAsUser] = useCurrentUser();
 
     useEffect(() => {
         fetch(`/api/recipes/${recipe.id}/tags`)
@@ -13,8 +15,25 @@ export default function RecipeCard({ recipe }: { recipe: recipeMeta }) {
             .then(data => setTags(data.tags));
     }, [recipe.id]);
 
+    const addToFavourites = () => {
+        fetchAsUser(`/api/users/${user.username}/collections/favourites`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ recipe_id: recipe.id })
+        }).then(res =>
+            res.ok
+                ? window.alert(`${recipe.name} was added to favourites!`)
+                : window.alert('try again:(')
+        );
+    };
+
     return (
         <div className='recipe-card' id={`${recipe.id}`}>
+            <div>
+                <span style={{ cursor: 'pointer' }} onClick={addToFavourites}>
+                    ❤️
+                </span>
+            </div>
             <div className='recipe-card-name'>
                 <NavLink to={`/recipes/${recipe.id}`}>{recipe.name}</NavLink>
             </div>
