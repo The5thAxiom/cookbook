4b2f80ec206942f429f5e9145b9d60a9f2a918e3
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-06-12 18:01:02 +0530

    renamed table

--- backend/models.py
+++ backend/models.py
@@ -163,7 +163,7 @@ class Tag(db.Model):
     name = db.Column(db.String(64), nullable=False)
 
 
-Collection_Recipe = db.Table('Collection_Recipe',
+Collection_Recipe = db.Table('collection_recipe',
                              db.Column(
                                  'id',
                                  db.Integer,
