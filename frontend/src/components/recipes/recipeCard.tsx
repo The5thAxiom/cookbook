f9116b52eceb1de17a9b007584e1cbddbb07f67d
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-06-12 12:09:41 +0530

    recipe in favourites can be removed from recipe card

--- frontend/src/components/recipes/recipeCard.tsx
+++ frontend/src/components/recipes/recipeCard.tsx
@@ -15,28 +15,66 @@ export default function RecipeCard({ recipe }: { recipe: recipeMeta }) {
             .then(data => setTags(data.tags));
     }, [recipe.id]);
 
+    const [userFavourites, setUserFavourites] = useState<recipeMeta[]>(
+        null as any
+    );
+
+    const fetchUserFavourites = () =>
+        fetchAsUser(`/api/users/${user.username}/collections/favourites`)
+            .then(res => res.json())
+            .then(data => setUserFavourites(data.recipes));
+
+    useEffect(() => {
+        if (user && recipe) fetchUserFavourites();
+    }, [user, recipe]);
+
     const addToFavourites = () => {
         fetchAsUser(`/api/users/${user.username}/collections/favourites`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ recipe_id: recipe.id })
-        }).then(res =>
-            res.ok
-                ? window.alert(`${recipe.name} was added to favourites!`)
-                : window.alert('try again:(')
-        );
+        }).then(res => {
+            if (res.ok) {
+                window.alert(`${recipe.name} was added to favourites!`);
+                fetchUserFavourites();
+            } else window.alert('try again:(');
+        });
+    };
+
+    const removeFromFavourites = () => {
+        fetchAsUser(`/api/users/${user.username}/collections/favourites`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ recipe_id: recipe.id })
+        }).then(res => {
+            if (res.ok) {
+                window.alert(`${recipe.name} was removed from favourites!`);
+                fetchUserFavourites();
+            } else window.alert('try again:(');
+        });
     };
 
     return (
         <div className='recipe-card' id={`${recipe.id}`}>
             {user && (
-                <div>
-                    <span
-                        className='util-clickable util-row-flexend'
-                        onClick={addToFavourites}
-                    >
-                        ❤️
-                    </span>
+                <div className=' util-row-flexend'>
+                    {userFavourites &&
+                    userFavourites.filter(r => r.id === recipe.id).length ===
+                        1 ? (
+                        <span
+                            className='util-clickable'
+                            onClick={removeFromFavourites}
+                        >
+                            X
+                        </span>
+                    ) : (
+                        <span
+                            className='util-clickable'
+                            onClick={addToFavourites}
+                        >
+                            ❤️
+                        </span>
+                    )}
                 </div>
             )}
             <div className='recipe-card-name'>
